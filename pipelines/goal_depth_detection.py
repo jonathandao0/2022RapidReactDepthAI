02cdb7406e898c325b0f0cfb105f9d8dc3a96294
Jonathan Dao <jonathandao0@gmail.com>
2022-03-10 11:23:36 -0800

    Added brightness setting to deal with arena lights

--- pipelines/goal_depth_detection.py
+++ pipelines/goal_depth_detection.py
@@ -59,6 +59,7 @@ def create_pipeline(model_name):
     # Temperature in Kelvins (1000-12000)
     camRgb.initialControl.setAutoWhiteBalanceMode(dai.CameraControl.AutoWhiteBalanceMode.OFF)
     camRgb.initialControl.setManualWhiteBalance(4000)
+    camRgb.initialControl.setBrightness(-2)
 
     resizeManip.setMaxOutputFrameSize(519168)   # 416*416*3
     resizeManip.initialConfig.setResizeThumbnail(NN_IMG_SIZE, NN_IMG_SIZE)
